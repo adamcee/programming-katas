175725aece2e616570095df6083e83cf7be8ec34
adamcee <adam.cahan@gmail.com>
2021-05-16 15:47:58 -0500

    Generalize frame comparison logic
    - Generalize the frame comparison logic we used to identify 0.
    - Create `isInteger()` which compares a frame to another frame we
    specify. Use it to compare an input frame against an IntegerFrame - a
    frame we know is valid and represents the desired integer.
    - Replace isZero() logic with isInteger() logic.
    - Confirm that "tests" (our console logs) pass.

--- main.js
+++ main.js
@@ -88,6 +88,24 @@ const ZERO = [
 ];
 
 console.log('ZERO IS ', JSON.stringify(ZERO));
+
+/*
+ * isInteger
+ * Returns true if the frame represents some integer, false if not.
+ * NOTE: We should not access this directly, but write wrappers for it.
+ *
+ * @param  {Frame} frame
+ *         Frame of OCR data.
+ * @param  {IntegerFrame} integerFrame
+ *         Valid Frame of OCR data representing our integer. We compare the input frame
+ *         against this valid frame..
+ * @return {Boolean}
+ *         Returns true if our OCR equals validFrame
+ */
+function isInteger(frame, integerFrame) {
+    return JSON.stringify(frame) === JSON.stringify(integerFrame)
+}
+
 /*
  * isZero
  * Returns true if the frame represents the integer 0, false if not.
@@ -97,7 +115,7 @@ console.log('ZERO IS ', JSON.stringify(ZERO));
  *         Returns true if our OCR equals ZERO
  */
 function isZero(frame) {
-    return JSON.stringify(frame) === JSON.stringify(ZERO)
+    return isInteger(frame, ZERO);
 }
 
 /**
