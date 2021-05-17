7513aab0e4f1cedf776c8a59c0204d190ee4672d
adamcee <adam.cahan@gmail.com>
2021-05-17 08:46:03 -0500

    Read in file successfully

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+fs = require('fs');
+
 console.log('hello world'); // TODO - delete
+
+// Read in a single entry
+const DATA_DIR = './data/'
+const VALID_DATA_DIR = `${DATA_DIR}valid/`;
+fs.readFile(`${VALID_DATA_DIR}single-entry.txt`, 'utf8', (error, data) => {
+    if(error) {
+        return console.log('Error reading file: ', error);
+    }
+
+    console.log(data);
+});
+
